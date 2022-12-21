a77b5223ec1d88329f7d352ea63f737fd428774a
Lokesh22105113 <120353498+Lokesh22105113@users.noreply.github.com>
2022-12-22 00:03:00 +0530

    q2.py

---
+++ q2.py
@@ -0,0 +1,6 @@
+name ='Lokesh Kumar'
+sid = '22105113'
+depatment_name = 'ECE'
+cgpa = '9'
+# printing a para using different variables.
+print(f"Hey,{name} Here! \nMy SID is {sid} \nI am from {depatment_name} department and my CGPA is {cgpa}")
